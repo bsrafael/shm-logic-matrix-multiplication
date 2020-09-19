6af11ae9dac832a52c2c14a1ff3f5b753c642678
bsrafael <rafaelbs159@gmail.com>
2020-09-19 20:30:50 -0300

    multiprocessos

--- matriz.c
+++ matriz.c
@@ -8,11 +8,8 @@
 #include <unistd.h>
 
 /*------------------------------------------------------------------*/
-#define SIZE 30
-
 #define N 4 // matriz NxN
-#define P 2 // numero de processos
-
+#define P 3 // numero de processos
 /*------------------------------------------------------------------*/
 
 void ler_matriz(int matriz[N][N])
@@ -95,14 +92,14 @@ int main()
   int mat2[N][N];
 
   printf("Multiplicação Lógica de Matrizes\n");
-  printf("Vamos multiplicar duas matrizes %d x %d\n", N, N);
-  printf("\nPrimeira matriz\n");
-  ler_matriz(mat1);
+  printf("Vamos multiplicar duas matrizes %d x %d, usando %d processos\n", N, N, P);
+  // printf("\nPrimeira matriz\n");
+  // ler_matriz(mat1);
 
-  printf("\nSegunda matriz\n");
-  ler_matriz(mat2);
+  // printf("\nSegunda matriz\n");
+  // ler_matriz(mat2);
 
-  // mock(mat1, mat2);
+  mock(mat1, mat2);
 
   printf("\n\nPrimeira matriz\n");
   escrever_matriz(mat1);
@@ -110,7 +107,7 @@ int main()
   printf("\nSegunda matriz\n");
   escrever_matriz(mat2);
 
-  int i, shmid, pid;
+  int i, p, shmid, pid;
   int chave = 1;
   int *memoria;
 
@@ -124,51 +121,35 @@ int main()
     printf("Erro na alocacao");
   }
 
-  pid = fork();
-
-  if (pid > 0) // pai
-  {
-    int xF, yF, i;
-
-    for (xF = 0; xF < N; xF += 2)
-    {
-      for (yF = 0; yF < N; yF++)
+  for (p = 0; p < P; p++) {
+    pid = fork();
+    if (pid == 0) {
+      int x, y, i;
+      for (x = p; x < N; x += P)
       {
-        int mins[N];
-        for (i = 0; i < N; i++)
+        for (y = 0; y < N; y++)
         {
-          mins[i] = min(mat1[xF][i], mat2[i][yF]);
+          int mins[N];
+          for (i = 0; i < N; i++)
+          {
+            mins[i] = min(mat1[x][i], mat2[i][y]);
+          }
+          memoria[x * N + y] = max_arr(mins, N);
         }
-        memoria[xF * N + yF] = max_arr(mins, N);
       }
+      shmdt(memoria);
     }
 
-    wait(NULL);
-    int final[N][N];
-    array_para_matriz(memoria, final);
-    printf("\n\nResultado:\n");
-    escrever_matriz(final);
-    shmdt(memoria);
-    shmctl(shmid, IPC_RMID, NULL);
   }
-  else // filho
-  {
-    int xP, yP, i;
 
-    for (xP = 1; xP < N; xP += 2)
-    {
-      for (yP = 0; yP < N; yP++)
-      {
-        int mins[N];
-        for (i = 0; i < N; i++)
-        {
-          mins[i] = min(mat1[xP][i], mat2[i][yP]);
-        }
-        memoria[xP * N + yP] = max_arr(mins, N);
-      }
-    }
-    shmdt(memoria);
-  }
+  wait(NULL);
+  int final[N][N];
+  array_para_matriz(memoria, final);
+
+  printf("\n\nResultado:\n");
+  escrever_matriz(final);
+  shmdt(memoria);
+  shmctl(shmid, IPC_RMID, NULL);
 
   return 0;
 }
